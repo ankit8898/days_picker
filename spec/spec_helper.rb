ef2289d2ac2434b80f3d3f03ebc01a6ac77b7fdd
Ankit Gupta <ankit.gupta8898@gmail.com>
2013-10-27 00:11:48 +0100

    Adding coveralls

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,7 +1,12 @@
 require 'rubygems'
+require 'json'
+require 'coveralls'
+
 require 'bundler/setup'
 
 require 'days_picker' # and any other gems you need
+
+Coveralls.wear!
 
 RSpec.configure do |config|
   # some (optional) config here
